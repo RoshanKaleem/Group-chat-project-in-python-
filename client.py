7f9f8588b43d2da5e590bbe68cfe9efb893dded4
Roshan Kaleem <65737218+RoshanKaleem@users.noreply.github.com>
2021-03-08 11:55:56 -0800

    Client uploaded

---
+++ client.py
@@ -0,0 +1,39 @@
+import socket
+import sys
+from threading import Thread
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+name = input("enter your username :")
+localIP = "127.0.0.1"
+UDPClientSocket.bind((localIP, 20002))
+
+
+def recv():
+    bytesToSend1 = str.encode(name +" : Joined the chat")
+    serverAddressPort = ("127.0.0.1", 20001)
+
+    bufferSize = 1024
+    UDPClientSocket.sendto(bytesToSend1, serverAddressPort)
+    while(1):
+
+     msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+     msg = "{}".format(msgFromServer[0].decode())
+
+     print(msg)
+
+Thread(target=recv).start()
+
+
+
+while(1):
+    msg = input()
+    bytesToSend1 = str.encode(name+":"+msg)
+
+    serverAddressPort = ("127.0.0.1", 20001)
+
+    bufferSize = 1024
+
+
+    UDPClientSocket.sendto(bytesToSend1, serverAddressPort)
+
